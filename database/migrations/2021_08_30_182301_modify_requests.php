fcbd35cd8a8fd1eb9376803154ac352bb0afe052
Vitaly <taneev@iswin.ru>
2021-08-30 21:41:58 +0300

    add install.sh

--- database/migrations/2021_08_30_182301_modify_requests.php
+++ database/migrations/2021_08_30_182301_modify_requests.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRequests2 extends Migration
+class ModifyRequests extends Migration
 {
     /**
      * Run the migrations.
